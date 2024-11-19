2deb4cfdb55ec3c2c80c1eaec206947575d5254f
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-19 11:12:05 +0530

    register edi service in program.cs

--- edi-315-parser-api/Program.cs
+++ edi-315-parser-api/Program.cs
@@ -1,13 +1,16 @@
 
+using edi_315_parser_api.Services;
+
 namespace edi_315_parser_api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddSingleton<IEDIParserService>(await InitializeCosmosClientInstanceAsync(builder.Configuration.GetSection("CosmosDb")));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -32,5 +35,21 @@ namespace edi_315_parser_api
 
             app.Run();
         }
+
+        private static async Task<EDIParserService> InitializeCosmosClientInstanceAsync(IConfigurationSection configurationSection)
+        {
+            var databaseName = configurationSection["DatabaseName"];
+            var containerName = configurationSection["ContainerName"];
+            var account = configurationSection["Account"];
+            var key = configurationSection["Key"];
+
+            var client = new Microsoft.Azure.Cosmos.CosmosClient(account, key);
+            var database = await client.CreateDatabaseIfNotExistsAsync(databaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(containerName, "/PartitionKey", 400);
+
+            var cosmosDbService = new EDIParserService(client, databaseName, containerName);
+            return cosmosDbService;
+        }
+
     }
 }
