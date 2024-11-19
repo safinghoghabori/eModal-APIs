98e39e1d90c02df877acb8d9a770fd93d043abeb
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-19 10:54:22 +0530

    add edi controller

---
+++ edi-315-parser-api/Controllers/EDIParserController.cs
@@ -0,0 +1,26 @@
+﻿using edi_315_parser_api.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace edi_315_parser_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EDIParserController : ControllerBase
+    {
+        private readonly IEDIParserService _ediParserService;
+        public EDIParserController(IEDIParserService ediParserService) 
+        {
+            _ediParserService = ediParserService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> UploadFile(IFormFile? file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest(new { message = "File is empty or not provided." });
+
+            await _ediParserService.ParseEDIFile(file);
+            return Ok(new {message = "successfully parsed."});
+        }
+    }
+}
