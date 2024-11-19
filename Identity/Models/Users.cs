bc529563c60181fa863ccca085d7de9cbaf32b69
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-19 14:01:54 +0530

    add users model

---
+++ Identity/Models/Users.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Identity.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(30)]
+        public string Password { get; set; }
+    }
+}
