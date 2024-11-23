f56632c948e3058896b9656455d58611260c2612
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-23 22:25:07 +0530

    add model

--- Payment.API/Models/PaymentConfirmation.cs
+++ Payment.API/Models/PaymentConfirmation.cs
@@ -4,7 +4,7 @@
     {
         public string UserId { get; set; }
         public string EdiFileId { get; set; }
-        public decimal Amount { get; set; }
+        public int Amount { get; set; }
         public bool IsSuccessful { get; set; }
         public string TransactionId { get; set; }
         public DateTime PaymentDate { get; set; }
