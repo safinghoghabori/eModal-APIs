5000f537c507799284583d5bae7d17cd90523647
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-23 18:54:46 +0530

    add models

---
+++ Payment.API/Models/PaymentConfirmation.cs
@@ -0,0 +1,12 @@
+﻿namespace Payment.API.Models
+{
+    public class PaymentConfirmation
+    {
+        public string UserId { get; set; }
+        public string EdiFileId { get; set; }
+        public decimal Amount { get; set; }
+        public bool IsSuccessful { get; set; }
+        public string TransactionId { get; set; }
+        public DateTime PaymentDate { get; set; }
+    }
+}
---
+++ Payment.API/Models/PaymentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Payment.API.Models
+{
+    public class PaymentRequest
+    {
+        public string UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string EdiFileId { get; set; }  
+    }
+}
