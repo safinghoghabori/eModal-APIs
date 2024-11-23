4847fd2c53d5d47b567c25c31a8a0c874aee8a9c
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-23 22:44:58 +0530

    register services in main method

---
+++ PaymentConfirmationConsumerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PaymentConfirmationConsumerApp.Services;
+
+namespace PaymentConfirmationConsumerApp
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Create a Host Builder for dependency injection
+            var host = CreateHostBuilder(args).Build();
+
+            // Get the AzureServiceBusClient instance from DI container
+            var serviceBusClient = host.Services.GetRequiredService<IAzureServiceBusClient>();
+
+            // Start receiving messages from Service Bus
+            await serviceBusClient.ReceiveMessagesAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+        .ConfigureServices((hostContext, services) =>
+        {
+            // Register services in the DI container
+            services.AddScoped<IAzureServiceBusClient, AzureServiceBusClient>();
+            services.AddScoped<IPaymentConfirmationHandler, PaymentConfirmationHandler>();
+        });
+
+    }
+}
---
+++ PaymentConfirmationConsumerApp/Services/PaymentConfirmationHandler.cs
@@ -0,0 +1,21 @@
+﻿using PaymentConfirmationConsumerApp.Models;
+
+namespace PaymentConfirmationConsumerApp.Services
+{
+    public class PaymentConfirmationHandler : IPaymentConfirmationHandler
+    {
+        //private readonly ICosmosDbService _cosmosDbService;
+        //private readonly ISqlDbService _sqlDbService;
+
+        //public PaymentConfirmationHandler(ICosmosDbService cosmosDbService, ISqlDbService sqlDbService)
+        //{
+        //    _cosmosDbService = cosmosDbService;
+        //    _sqlDbService = sqlDbService;
+        //}
+
+        public async Task HandlePaymentConfirmationAsync(PaymentConfirmation paymentConfirmation)
+        {
+
+        }
+    }
+}
