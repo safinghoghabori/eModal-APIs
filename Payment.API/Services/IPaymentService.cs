b79b6330a84a81033038e4eac271b5d4c0699a14
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-23 21:03:50 +0530

    create payment service file to handle payment request

---
+++ Payment.API/Services/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using Payment.API.Models;
+
+namespace Payment.API.Services
+{
+    public interface IPaymentService
+    {
+        Task<PaymentConfirmation> InitiatePaymentAsync(PaymentRequest paymentRequest);
+    }
+}
---
+++ Payment.API/Services/PaymentService.cs
@@ -0,0 +1,34 @@
+﻿using Payment.API.Models;
+
+namespace Payment.API.Services
+{
+    public class PaymentService: IPaymentService
+    {
+        private readonly IPaymentProcessor _paymentProcessor;
+        private readonly IAzureServiceBusClient _azureServiceBusClient;
+
+        public PaymentService(IPaymentProcessor paymentProcessor, IAzureServiceBusClient azureServiceBusClient)
+        {
+            _paymentProcessor = paymentProcessor;
+            _azureServiceBusClient = azureServiceBusClient;
+        }
+
+        public async Task<PaymentConfirmation> InitiatePaymentAsync(PaymentRequest paymentRequest)
+        {
+            try
+            {
+                // Process the payment
+                var paymentConfirmation = await _paymentProcessor.ProcessPaymentAsync(paymentRequest);
+
+                // Send payment confirmation to Azure Service Bus Queue
+                await _azureServiceBusClient.SendMessageToQueueAsync(paymentConfirmation);
+
+                return paymentConfirmation;
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Payment initiation failed.", ex);
+            }
+        }
+    }
+}
