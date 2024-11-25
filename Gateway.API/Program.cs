367f346004c6920a64139e630d5ec26c1c8ec89e
safinghoghabori <safinghoghabori65@gmail.com>
2024-11-25 22:25:39 +0530

    register ocelot in main file

--- Gateway.API/Program.cs
+++ Gateway.API/Program.cs
@@ -1,33 +1,28 @@
 
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 namespace Gateway.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+
+            // Add Ocelot
+            builder.Services.AddOcelot();
+
+            // Add Ocelot.json as a configuration file
+            builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
+            // Enable Ocelot Middleware
+            await app.UseOcelot();
             app.MapControllers();
 
             app.Run();
